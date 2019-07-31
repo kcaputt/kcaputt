e5bc65fbbf05e7078aef0ed8f16e894a292cceb5
Minion3665 <pixtopia@null.net>
2019-07-31 19:37:29 +0100

    Update __init__.py

--- discordbots/minibot/tea/__init__.py
+++ discordbots/minibot/tea/__init__.py
@@ -12,7 +12,7 @@ class Tea(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 	
-	@bot.event
+	@commands.event
 	async def on_message(message):
 		if message.content.lower().startswith("hi dad im "):
 			await sendembed(message.channel, "Hi "+message.content[10:]+", I'm DAD!")
