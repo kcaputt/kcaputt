2533222446c9e2a900d1608d03c7de337c76251a
Minion3665 <pixtopia@null.net>
2019-07-31 17:31:13 +0100

    Update __init__.py

--- discordbots/minibot/__init__.py
+++ discordbots/minibot/__init__.py
@@ -1,1 +1,19 @@
+from discord.ext import commands
+import asyncio
+import os
 
+try:
+  bot = commands.Bot(command_prefix="mini")
+  for extension in extensions:
+    try:
+      bot.load_extension(extension)
+    except Exception as e:
+      print("Failed to load "+str(extension)+"... \n>>> Full traceback is "+str(e))
+  bot.run(os.environ["MINIBOT_TOKEN"])
+except Exception as e:
+  print("Running MiniBot failed... \n>>> Full traceback is "+str(e))
+  
+
+loop = asyncio.get_event_loop()
+
+loop.create_task(setup())
