989568d1774830a8e116a5e3b9c76a23bcb4abf3
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-22 11:27:32 +0100

    Update __init__.py

--- discordbots/activitycheck/main/__init__.py
+++ discordbots/activitycheck/main/__init__.py
@@ -27,19 +27,19 @@ def getCheckChannel(ctx):
 			return channel
 	return None
 
-@commands.on_command_error()
-async def test_error(ctx, error):
-	if isinstance(error, commands.MissingPermissions):
-		await sendEmbed(ctx, "Oops, we got an error", "**You don't seem to have enough permissions to run that command**\n\nAnd here is the error in person to tell you what it thinks...\n`"+str(error)+"`\n*(There- wasn't that just delightful)*")
-	elif isinstance(error, commands.BotMissingPermissions):
-		await sendEmbed(ctx, "Oops, we got an error", "**I don't seem to have enough permissions to run that command**\n\nAnd here is the error in person to tell you what it thinks...\n`"+str(error)+"`\n*(There- wasn't that just delightful)*")
-	elif isinstance(error, commands.NoPrivateMessage):
-		await sendEmbed(ctx, "Oops, we got an error", "**You can't run this one in DMs**\n\nAnd here is the error in person to tell you what it thinks...\n`"+str(error)+"`\n*(There- wasn't that just delightful)*")
-
 class ActivityChecks(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+	@commands.on_command_error()
+	async def test_error(ctx, error):
+		if isinstance(error, commands.MissingPermissions):
+			await sendEmbed(ctx, "Oops, we got an error", "**You don't seem to have enough permissions to run that command**\n\nAnd here is the error in person to tell you what it thinks...\n`"+str(error)+"`\n*(There- wasn't that just delightful)*")
+		elif isinstance(error, commands.BotMissingPermissions):
+			await sendEmbed(ctx, "Oops, we got an error", "**I don't seem to have enough permissions to run that command**\n\nAnd here is the error in person to tell you what it thinks...\n`"+str(error)+"`\n*(There- wasn't that just delightful)*")
+		elif isinstance(error, commands.NoPrivateMessage):
+			await sendEmbed(ctx, "Oops, we got an error", "**You can't run this one in DMs**\n\nAnd here is the error in person to tell you what it thinks...\n`"+str(error)+"`\n*(There- wasn't that just delightful)*")
+		
 	@commands.command(name="setup")
 	@commands.guild_only()
 	@commands.has_permissions(manage_roles=True, manage_channels=True)
