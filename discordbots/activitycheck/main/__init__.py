ca2f4d19a953794a44995f9da075bdbbafc0d0ed
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-22 08:19:27 +0100

    Update __init__.py

--- discordbots/activitycheck/main/__init__.py
+++ discordbots/activitycheck/main/__init__.py
@@ -3,10 +3,19 @@ import discord
 import os
 import asyncio
 
-async def sendembed(ctx, title, text=None, color=0x7289da):
+async def sendembed(ctx, title, text=None, color=0x7289da, message=""):
 	embed = discord.Embed(title = title, description = text, color = color)
 	embed.set_footer(text = "Made By Minion3665")
-	return await ctx.send(embed = embed)
+	return await ctx.send(message, embed = embed)
+
+async def changeEmbedColor(message, color=0x7289da):
+	newEmbed = None
+	try:
+		newEmbed = message.embeds[0].copy()
+	except:
+		pass
+	newEmbed.color = color
+	await message.edit(embed = newEmbed)
 
 def getCheckChannel(ctx):
 	for channel in ctx.guild.channels:
@@ -34,10 +43,10 @@ class ActivityChecks(commands.Cog):
 			await sendembed(ctx, "Your server is incorrectly setup", "Please run the "+ctx.prefix+"setup command to setup your server again", 0xaa0000)
 			return
 		myResponseMessage = await sendembed(ctx, "Loading activity check", "This may take a while... This embed will turn green when I start... Once the check has started you can send `stop` to the channel to stop the check early. A bot admin can also stop/mass-stop the check early, for example if maintenance is needed on the bot.")
-		await asyncio.sleep(5)
-		newEmbed = myResponseMessage.embeds[0].copy()
-		newEmbed.color = 0x6cb83a
-		await myResponseMessage.edit(embed = newEmbed)
+		for member in ctx.guild.members:
+			await member.remove_roles(activeRole, closedRole, reason="Activity Check Starting...")
+		await sendEmbed(activityCheckChannel, "Activity Check", "Type `me`. Nothing More. Nothing Less. All messages are due within 1 day of this being sent. I will react with ✅ if it worked.", message="@everyone")
+		changeEmbedColor(myResponseMessage, 0x6cb83a)
 
 def setup(bot):
 	bot.add_cog(ActivityChecks(bot))
