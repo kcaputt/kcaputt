2d963f1d4345ac5874c2123a4159c21fc8aa7607
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-22 13:47:14 +0100

    Update __init__.py

--- discordbots/activitycheck/main/__init__.py
+++ discordbots/activitycheck/main/__init__.py
@@ -32,7 +32,7 @@ def has_permissions(**perms):
 		ch = ctx.channel
 		permissions = ch.permissions_for(ctx.author)
 		missing = [perm for perm, value in perms.items() if getattr(permissions, perm, None) != value]
-		if not missing or ctx.bot.is_owner(ctx.author):
+		if not missing or await ctx.bot.is_owner(ctx.author):
 			return True
 		raise commands.MissingPermissions(missing)
 	return commands.check(predicate)
